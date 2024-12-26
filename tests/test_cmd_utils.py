f7f41708b2dc91d5f32b9cebf58d0fa761be15a4
Jussi-Pekka Erkkila <jp.erkkila@gmail.com>
2024-12-27 00:29:01 +0200

    constant values for tests

--- tests/test_cmd_utils.py
+++ tests/test_cmd_utils.py
@@ -1,6 +1,7 @@
 from unittest import mock, TestCase
 from secheaders import cmd_utils
 from secheaders.constants import WARN_COLOR, OK_COLOR, END_COLOR
+from .constants import EXAMPLE_HEADERS
 
 from .mock_classes import MockHTTPSConnection
 
@@ -15,26 +16,12 @@ class TestCmdUtils(TestCase):
         assert cmd_utils.get_eval_output(False, False) == f"[ {OK_COLOR}OK{END_COLOR} ]"
 
     def test_cmd_output(self) -> None:
-        example_headers = {
-            'server': 'nginx',
-            'x-xss-protection': '1;',
-        }
-        example_headers = {
-            'x-frame-options': {'defined': False, 'warn': True, 'contents': None, 'notes': []},
-            'strict-transport-security': {'defined': False, 'warn': True, 'contents': None, 'notes': []},
-            'content-security-policy': {'defined': False, 'warn': True, 'contents': None, 'notes': []},
-            'x-content-type-options': {'defined': False, 'warn': True, 'contents': None, 'notes': []},
-            'x-xss-protection': {'defined': True, 'warn': True, 'contents': '1;', 'notes': []},
-            'referrer-policy': {'defined': False, 'warn': True, 'contents': None, 'notes': []},
-            'permissions-policy': {'defined': False, 'warn': True, 'contents': None, 'notes': []},
-            'server': {'defined': True, 'warn': False, 'contents': 'nginx', 'notes': []},
-        }
         example_https = {
             'supported': True,
             'certvalid': True,
             'redirect': False,
         }
-        res = cmd_utils.output_text("example.com", example_headers, example_https, verbose=True, no_color=False)
+        res = cmd_utils.output_text("example.com", EXAMPLE_HEADERS, example_https, verbose=True, no_color=False)
         assert "HTTPS supported" in res
         assert "Scanning target example.com ..." in res
         assert "Header 'x-frame-options' is missing" in res
